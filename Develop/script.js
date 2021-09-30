97053ef4193fde7bb9cdc489de7e2f488e30194b
ZBreezie <zpmc55@yahoo.com>
2021-09-30 17:56:50 -0400

    -description class, +text class in textarea, function for dynamic time color change, changed color classes fonts to black

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,47 @@
-var ticker = setInterval(clockTicker, 1000);
+function timeCompare() {
+    // gets and logs current hour in military time
+    var currentHour = moment().hour();
+    console.log(currentHour)
 
+    // search for the text class in each time block and convert the ID to an integer 
+    $(".text").each(function(){
+        var scheduleHour = parseInt($(this).attr("id"))
+        console.log(scheduleHour)
+
+        // compare the integer of the ID to the current hour and swap classes accordingly
+        
+        // turn grey for "past"
+        if (scheduleHour < currentHour) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+        }
+        // turn red for "present"
+        else if (scheduleHour == currentHour) {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+            $(this).addClass("present");
+        }
+        // turn green for "future"
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+
+        }
+
+    })
+}
+
+
+
+// sets ticker for clock ticker function
+var ticker = setInterval(clockTicker, 1000);
+// displays the current time in current day ID
 function clockTicker(){
     var date = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     document.getElementById("currentDay").innerHTML = "Current date: " + date
 }
 
 clockTicker()
+timeCompare()
